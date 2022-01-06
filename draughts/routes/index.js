eccb53b1216be226f295bda439f3a19e5ec4337a
Thomas Voss <thomasvoss@live.com>
2022-01-06 16:24:38 +0100

    Use tabs, not spaces like some fucking heretic

--- draughts/routes/index.js
+++ draughts/routes/index.js
@@ -2,19 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get("/", function(req, res) {
-  res.sendFile("splash.html", { root: "./public" });
+router.get("/", (req, res) => {
+	res.sendFile("splash.html", { root: "./public" });
 });
 
 /* Pressing the 'PLAY' button, returns this page */
-router.get("/play", function(req, res) {
-  res.sendFile("game.html", { root: "./public" });
+router.get("/play", (req, res) => {
+	res.sendFile("game.html", { root: "./public" });
 });
 
 module.exports = router;
-
-
-
-
-
-
