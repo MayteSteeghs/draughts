1987fb57136f9534899bcf9f07737a989cf29674
Thomas Voss <thomasvoss@live.com>
2022-01-10 00:17:46 +0100

    Be a bit more verbose in message descriptions

--- draughts/public/javascripts/messages.js
+++ draughts/public/javascripts/messages.js
@@ -1,5 +1,5 @@
 (function (exports) {
-	exports.START = 0      // Server -> Client :: The game has started
-	exports.RESIGN = 1     // Server -> Client :: The opponent resigned
-	exports.DISCONNECT = 2 // Server -> Client :: The opponent disconnected
+	exports.START = 0      // Server  -> Client :: Inform the client that the game has started
+	exports.RESIGN = 1     // Server <-> Client :: Inform the server/opponent that the client resigned
+	exports.DISCONNECT = 2 // Server  -> Client :: Inform the client that the opponent disconnected
 })(typeof(exports) == "undefined" ? (this.Messages = {}) : exports)
