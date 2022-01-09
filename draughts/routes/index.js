8eb13e7ca7350418535eb7a163aeccd73bbf8a1e
Thomas Voss <thomasvoss@live.com>
2022-01-09 23:34:36 +0100

    Remove semicolons
    They are optional in javascript, so why waste screen space on them?

--- draughts/routes/index.js
+++ draughts/routes/index.js
@@ -1,14 +1,8 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router()
+const root = "./public"
 
 /* GET home page */
-router.get("/", (req, res) => {
-	res.sendFile("splash.html", { root: "./public" });
-});
+router.get("/", (_, res) => res.sendFile("splash.html", { root: root }))
+router.get("/play", (_, res) => res.sendFile("game.html", { root: root }))
 
-/* Pressing the 'PLAY' button, returns this page */
-router.get("/play", (req, res) => {
-	res.sendFile("game.html", { root: "./public" });
-});
-
-module.exports = router;
+module.exports = router
