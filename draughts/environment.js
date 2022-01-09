e18aae4ccc49a6c91032e8719281460ccc63346a
Thomas Voss <thomasvoss@live.com>
2022-01-09 23:37:03 +0100

    Create an environment object
    The environment object holds basically all the information about the
    state of all the games, statistics regarding games, etc.
    
    The `minimumMoves` and `averageMoves` variables start out at infinity
    just so that we have an easy way to check if they've actually been set
    yet or not, since they won't be properly set before any games have
    even been played.

---
+++ draughts/environment.js
@@ -0,0 +1,10 @@
+/* An object representing the complete environment. It holds all the games as well as some statistics
+ * to be displayed on the splash screen.
+ */
+const environment = {
+	minimumMoves: Infinity,
+	averageMoves: Infinity,
+	games: []
+}
+
+module.exports = environment
