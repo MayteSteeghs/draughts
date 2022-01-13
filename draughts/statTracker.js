e448ee4e94fcd18bfca31fbc89723e394a964b54
Thomas Voss <thomasvoss@live.com>
2022-01-13 22:41:34 +0100

    Fix indentation

--- draughts/statTracker.js
+++ draughts/statTracker.js
@@ -1,9 +1,8 @@
 const gameStatus = {
 	since: Date.now() /* since we keep it simple and in-memory, keep track of when this object was created */,
 	minimumMoves: Infinity,
-	  averageMoves: Infinity,
-	  totalGames: 0
-  };
+	averageMoves: Infinity,
+	totalGames: 0
+}
   
-  module.exports = gameStatus;
-  
+module.exports = gameStatus;
