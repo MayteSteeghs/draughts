2d6984ec55c0b2aa51096ec8f7a8d43285fb9bec
Thomas Voss <thomasvoss@live.com>
2022-01-17 00:58:26 +0100

    More documentation

--- draughts/statTracker.js
+++ draughts/statTracker.js
@@ -2,10 +2,10 @@
  * stats get displayed on the splash screen.
  */
 var gameStatus = {
-	minimumMoves: Infinity,
-	averageMoves: Infinity,
-	ongoingGames: 0,
-	totalGames: 0
+	minimumMoves: Infinity, // The minimum amount of moves a game was completed in
+	averageMoves: Infinity, // The average amount of moves games are completed in
+	ongoingGames: 0,        // The number of ongoing games
+	totalGames: 0           // The number of total games started (used for calculations)
 }
   
 module.exports = gameStatus;
